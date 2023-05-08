11832f3e7dd16dc786a6ebfe23c14616741c9aa3
meta_panic <margo.sockolova2010@yandex.ru>
2023-05-08 15:59:18 +0400

    Emoji is role=presentation now

--- src/components/FruitComponent/FruitComponent.tsx
+++ src/components/FruitComponent/FruitComponent.tsx
@@ -1,12 +1,16 @@
 import styles from "./FruitComponent.module.scss";
 import React from "react";
-import {Fruit} from "../../pages/demo/types.ts";
+import { Fruit } from "../../pages/demo/types.ts";
 
 export const FruitComponent = ({ name, emoji }: Fruit) => {
-    return (
-        <div className={styles.fruit}>
-            {emoji && <div className={styles.emojiWrapper}>{emoji}</div>}
-            {name}
+  return (
+    <div className={styles.fruit}>
+      {emoji && (
+        <div className={styles.emojiWrapper} role={"presentation"}>
+          {emoji}
         </div>
-    );
+      )}
+      {name}
+    </div>
+  );
 };
